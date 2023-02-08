18cb68395c4c565077c24cdae5fdd9b47f371b67
unknown <devlyon6@academy.priv>
2023-02-08 14:24:00 +0100

    creation des premieres migrations

---
+++ backend/database/migrations/2023_02_08_093637_create_veut_garder_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('veut_garder', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_plante');
+            $table->integer('id_gardien');
+            $table->integer('id_proprietaire');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('veut_garder');
+    }
+};
