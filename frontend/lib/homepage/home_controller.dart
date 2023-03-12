9edfc18aa68baebee6863472bd89e5fb978b5505
bilal69000 <89282371+bilal69000@users.noreply.github.com>
2023-03-12 23:43:02 +0100

    ficx

--- frontend/lib/homepage/home_controller.dart
+++ frontend/lib/homepage/home_controller.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart' hide Response;
 
 import '../model/plante_list/plante.dart';
 import '../model/user/user.dart';
@@ -8,7 +8,7 @@ import 'package:flutter/widgets.dart';
 class HomeController extends GetxController {
   late User user;
   final planteList = <Plante>[].obs;
-
+  final RxStatus status = RxStatus.loading();
   void onInit() async {
     super.onInit();
     final Map<String, dynamic> args = Get.arguments;
@@ -18,12 +18,14 @@ class HomeController extends GetxController {
 
   Future<void> fetchPlantes() async {
     try {
-      final response = await Dio().get('http://10.0.2.2:8000/api/plante');
-      final data = response.data as List<dynamic>;
-      final plantes = data.map((e) => Plante.fromJson(e)).toList();
-      planteList.addAll(plantes);
-    } catch (e) {
-      print(e);
+      status.isLoading;
+      Response response = await Dio().get('http://10.0.2.2:8000/api/plante');
+      List<dynamic> data = response.data;
+      planteList.value = data.map((plant) => Plante.fromJson(plant)).toList();
+      status.isSuccess;
+    } catch (error) {
+      status.isError;
+      print(error);
     }
   }
 }
--- frontend/lib/homepage/home_view.dart
+++ frontend/lib/homepage/home_view.dart
@@ -6,35 +6,30 @@ import 'home_controller.dart';
 
 class Home extends GetView<HomeController> {
   @override
-  Widget build(context) => Scaffold(
-        backgroundColor: Color(0xFFD6FFCC),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                  height: 200,
-                  width: double.infinity,
-                  // child: SvgPicture.asset(
-                  //   'svg/bottanic_home.svg',
-                  //   width: 100,
-                  //   height: 100,
-                  // ),
-                  child: Obx(() {
-                    final plantes = controller.planteList;
-                    return ListView.builder(
-                      itemCount: plantes.length,
-                      itemBuilder: (context, index) {
-                        final plante = plantes[index];
-                        return ListTile(
-                          title: Text(plante.nomPlante ?? ""),
-                          subtitle: Text(plante.description ?? ""),
-                          trailing: Text(plante.image ?? ""),
-                        );
-                      },
-                    );
-                  }))
-            ],
-          ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFD6FFCC),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: controller.planteList.length,
+                itemBuilder: (context, index) {
+                  final plante = controller.planteList[index];
+                  return ListTile(
+                    title: Text(plante.nomPlante ?? ""),
+                    subtitle: Text(plante.localisation ?? ""),
+                    leading: Image.network(plante.image ?? ""),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
