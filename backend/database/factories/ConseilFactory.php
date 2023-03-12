6dcb2398e57766e25938605c5de3927ab9e53679
Ibrahimch69 <96245990+Ibrahimch69@users.noreply.github.com>
2023-03-12 15:12:26 +0100

    creation des seeder et des factory

---
+++ backend/database/factories/ConseilFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Conseil>
+ */
+class ConseilFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'plante_id' => $this->faker->numberBetween(1, 10),
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'titreConseil' => $this->faker->name(),
+            'contenuConseil' => $this->faker->name(),
+        ];
+    }
+}
