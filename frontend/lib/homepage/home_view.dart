c40bc31a9b27242f9f21e2cdaafc27fe843d978e
bilal69000 <89282371+bilal69000@users.noreply.github.com>
2023-03-18 20:37:19 +0100

    finished

--- frontend/lib/homepage/home_view.dart
+++ frontend/lib/homepage/home_view.dart
@@ -8,26 +8,55 @@ class Home extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFD6FFCC),
+      backgroundColor: const Color(0xFFD6FFCC),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              width: double.infinity,
-              child: ListView.builder(
+        child: Expanded(
+          child: Column(
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
                 itemCount: controller.planteList.length,
                 itemBuilder: (context, index) {
                   final plante = controller.planteList[index];
-                  return ListTile(
-                    title: Text(plante.nomPlante ?? ""),
-                    subtitle: Text(plante.localisation ?? ""),
-                    leading: Image.network(plante.image ?? ""),
-                  );
+                  return Column(children: [
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        const SizedBox(width: 16),
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundImage: NetworkImage(plante.image ?? ""),
+                        ),
+                        const SizedBox(width: 16),
+                        Text(
+                          plante.nomPlante ?? "",
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 200,
+                      child: Image.network(plante.image ?? ""),
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 24,
+                      child: IconButton(
+                        icon: const Icon(Icons.comment),
+                        onPressed: () {},
+                      ),
+                    )
+                  ]);
                 },
               ),
-            ),
-          ],
+              SizedBox(
+                height: 50,
+              )
+            ],
+          ),
         ),
       ),
     );
