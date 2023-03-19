1ba9e81423e1a417d62fcaa01c7ba6d5851bb919
bilal69000 <89282371+bilal69000@users.noreply.github.com>
2023-03-19 13:20:24 +0100

    fix

--- frontend/lib/homepage/home_view.dart
+++ frontend/lib/homepage/home_view.dart
@@ -12,58 +12,63 @@ class Home extends GetView<HomeController> {
     return Scaffold(
       backgroundColor: const Color(0xFFD6FFCC),
       body: SingleChildScrollView(
-        child: Expanded(
-          child: Column(
-            children: [
-              Obx(() => ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: controller.planteList.length,
-                    itemBuilder: (context, index) {
-                      final plante = controller.planteList[index];
-                      return Column(children: [
-                        const SizedBox(height: 16),
-                        Row(
-                          children: [
-                            const SizedBox(width: 16),
-                            CircleAvatar(
-                              radius: 20,
-                              backgroundImage:
-                                  NetworkImage(plante.photoProfil ?? ""),
-                            ),
-                            const SizedBox(width: 16),
-                            Text(
-                              "${plante.prenomUtilisateur} ${plante.nameUtilisateur}",
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 16),
-                        SizedBox(
-                          height: 200,
-                          child: Image.network(plante.image ?? ""),
-                        ),
-                        const SizedBox(height: 16),
-                        SizedBox(
-                          height: 24,
-                          child: GestureDetector(
-                            child: Icon(Icons.comment),
-                            onTap: () {
-                              Get.to(() => PostPage(),
-                                  binding: PostPageBindings(),
-                                  arguments: {"planteList": plante});
-                            },
+        child: Column(
+          children: [
+            Obx(
+              () => ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: controller.planteList.length,
+                itemBuilder: (context, index) {
+                  final plante = controller.planteList[index];
+                  return Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      Row(
+                        children: [
+                          const SizedBox(width: 16),
+                          CircleAvatar(
+                            radius: 20,
+                            backgroundImage:
+                                NetworkImage(plante.photoProfil ?? ""),
                           ),
+                          const SizedBox(width: 16),
+                          Text(
+                            "${plante.prenomUtilisateur} ${plante.nameUtilisateur}",
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        height: 200,
+                        child: Image.network(
+                          plante.image ?? "",
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Icon(Icons.broken_image, size: 200);
+                          },
                         ),
-                      ]);
-                    },
-                  )),
-              SizedBox(
-                height: 50,
-              )
-            ],
-          ),
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        height: 24,
+                        child: GestureDetector(
+                          child: Icon(Icons.comment),
+                          onTap: () {
+                            Get.to(() => PostPage(),
+                                arguments: {"planteList": plante});
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 50),
+          ],
         ),
       ),
     );
